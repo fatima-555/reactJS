a545bc04f2e27967117528d2918a54d1e71aa1dd
fatima mustufa <fatimamustufa6@gmail.com>
2024-12-15 16:55:26 -0800

    react js project

---
+++ first project/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+const rootDiv = document.getElementById("root");
+
+ReactDom.createRoot(rootDiv).render(
+  <div>
+    <h1>Hello ReactJS</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Mollitia quisquam nostrum, reiciendis harum ratione fuga laudantium ex veritatis dolorum omnis magnam libero, 
+    sunt voluptatem suscipit illo aliquid! Molestiae quisquam cupiditate beatae dicta error, 
+    possimus nemo ut temporibus amet aliquam voluptates accusamus maiores placeat ducimus pariatur numquam dolore! 
+    Earum similique quo voluptatem! Doloribus, 
+    voluptatem nisi deserunt perferendis veniam repudiandae nobis consectetur fugit porro laudantium alias atque nostrum maxime qui praesentium distinctio voluptas obcaecati delectus? 
+    Obcaecati quaerat perferendis dolorem hic omnis vitae ex nobis recusandae rerum cum at vero aperiam, impedit qui pariatur corrupti 
+    necessitatibus ipsam possimus autem nam fugit dolores sed! 
+    Perspiciatis blanditiis architecto sequi neque earum alias quasi.
+    Quaerat quisquam harum maxime, in sed iusto voluptates officia blanditiis
+    commodi natus totam modi sunt ex ut laboriosam ad molestiae fuga est 
+    mollitia. Alias magnam adipisci consequuntur impedit sed non sunt natus nam aliquam 
+    aperiam laudantium, tempora quis quibusdam porro recusandae inventore, consectetur error? 
+    Magni officiis velit nobis, error optio est? Perspiciatis dicta vel ullam 
+    dolorem deleniti, rem assumenda ab, sunt quae doloremque reprehenderit 
+    reiciendis non inventore eaque iusto at magnam enim! Nemo nobis iste esse 
+    quasi porro! Laborum porro beatae minus aperiam, quae similique ut rerum 
+    esse, provident expedita, quibusdam vel.</p>
+    <img src="/public/vite.svg" alt="" />
+    <img src="/src/assets/react.svg" alt="" />
+    <a href="#">Learn More</a>
+
+  </div>
+)
